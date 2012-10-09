68616809705453fad5882612cd383aa7753c1948
Mohan Kishore <mohankishore@yahoo.com>
2012-10-09 10:11:56 -0700

    Fixed the Main class to actually process the command line arguments

--- src/main/java/com/github/mohankishore/jgrep/Main.java
+++ src/main/java/com/github/mohankishore/jgrep/Main.java
@@ -12,25 +12,32 @@ public class Main {
      *    classpath : standard java classpath 
      *                e.g. "./lib/*:./classes" 
      *    pattern   : <package>/<class>.<member>(<param-types>)<return-type>
-     *                e.g. "com/github/mohankishore/grep/Main.main([Ljava/lang/String;)V"
      *                e.g. "com/github/mohankishore/"
+     *                e.g. "com/github/mohankishore/grep/Main.main([Ljava/lang/String;)V"
      *    -e        : By default, the pattern match is a simple str.startsWith(p)
-     *                When -e is specified, the strings are interpreted as regex
-     *                expressions and used as Pattern.compile(p).matcher(str).matches() 
+     *                When -e is specified, the strings are interpreted as regex expressions
+     *                and used as Pattern.compile(p).matcher(str).matches() 
      *                e.g. "java\\/io\\/.*\\.close\\(.*"           
      *  
      * @param args
      * @throws Exception
      */
 	public static void main(String[] args) throws Exception {
+        if (args == null || args.length == 0) return;
+
         JGrep jgrep = new JGrep();
 	    String classpath = System.getProperty("java.class.path");
 	    for (String path : classpath.split(File.pathSeparator)) {
 	        jgrep.addClassPath(path);
 	    }
-	    //jgrep.setRegexMode(true);
-		//jgrep.addPattern("java\\/io\\/.*\\.close\\(.*");
-	    jgrep.addPattern("java/util/");
+	    int ndx = 0;
+	    if (args[0].equals("-e")) {
+	        jgrep.setRegexMode(true);
+	        ndx = 1;
+	    }
+	    for (; ndx < args.length; ndx++) {
+	        jgrep.addPattern(args[ndx]);
+	    }
 		jgrep.execute();
 	}
 
